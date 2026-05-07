3e35fa7a5548f539233e9172604ad887daceaa00
Wayfleh <rafael.caculba04@gmail.com>
2026-05-07 01:39:06 -0700

    transitions are now in place
    I think they're a little fast, kinda hurts my eyes, but I'll be fixing it
    
    I don't want to sleep

--- untitled-vampire-game/Misc/opening_cutscene.gd
+++ untitled-vampire-game/Misc/opening_cutscene.gd
@@ -4,10 +4,11 @@ extends Control
 @export var scroll_speed: int = 100
 
 var curr_scroll: float = 0
+var button_pressed: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	button_pressed = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 	
 func _process(delta: float) -> void:
@@ -16,6 +17,10 @@ func _process(delta: float) -> void:
 		scroll.scroll_vertical += 1
 		curr_scroll = 0
 	
-	if (scroll.scroll_vertical >= scroll.get_v_scroll_bar().max_value - scroll.get_v_scroll_bar().page
-	or Input.is_anything_pressed()):
+	if scroll.scroll_vertical >= scroll.get_v_scroll_bar().max_value - scroll.get_v_scroll_bar().page:
+		GlobalData.next_level()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey && !button_pressed:
+		button_pressed = true
 		GlobalData.next_level()
